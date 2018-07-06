a496a1911a972e02cee65b23e180b82d4482d23f
Hasiotis Nikos <hasiotis@gmail.com>
2018-07-06 12:36:35 +0300

    Just in case we need to delete infra as well

--- azure/az-teardown.sh
+++ azure/az-teardown.sh
@@ -2,3 +2,4 @@
 
 az ad sp delete --id `az ad sp list --query "[?displayName == 'ExternalDnsServicePrincipal'].appId" -o tsv`
 az group delete -n production --yes
+#az group delete -n infrastructure --yes
